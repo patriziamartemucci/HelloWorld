2e80c7bc3fd2ef836bed8518d27afdd7febb6b39
patriziamartemucci <martemucci.patrizia@gmail.com>
2023-04-16 18:35:52 +0200

    ciao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Patrizia';
+  title = 'Primo programma in Angular';
+  nome = 'Patrizia';
   cognome = 'Martemucci';
 }
